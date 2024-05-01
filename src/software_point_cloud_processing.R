f8fa9049fa837163cee8f117df5c788244aa4862
georgewoolsey <george.woolsey@colostate.edu>
2024-05-01 14:10:07 -0600

    statistical analysis and validation data

--- src/software_point_cloud_processing.R
+++ src/software_point_cloud_processing.R
@@ -2948,19 +2948,20 @@ process_raw_las_fn = function(my_las_file_path){
   
   las_list_df %>% 
     dplyr::filter(
-      !(processing_attribute2 %in% c("ORIGINAL") & processing_attribute3 %in% c("HIGH"))
-      & !(file_name %in% c("QUARTER_ORIGINAL_OPTIMAL" , "HALF_ORIGINAL_LOW", "HALF_ORIGINAL_OPTIMAL"))
+      (study_site== "N1" & file_name == "QUARTER_ORIGINAL_HIGH") # chunking too small
     ) %>% 
     View()
 
   # map over function for all las files
   processed_tracking_data = las_list_df %>%
     dplyr::filter(
-      !(processing_attribute2 %in% c("ORIGINAL") & processing_attribute3 %in% c("HIGH"))
-      & !(file_name %in% c("QUARTER_ORIGINAL_OPTIMAL" , "HALF_ORIGINAL_LOW", "HALF_ORIGINAL_OPTIMAL"))
-      # (!processing_attribute2 %in% c("DISABLED"))
-      # & (!study_site %in% c("KAIBAB_HIGH","KAIBAB_LOW","N1"))
-      # & (!study_site %in% c("KAIBAB_HIGH","KAIBAB_LOW","N1"))
+      (study_site== "N1" & file_name == "QUARTER_ORIGINAL_HIGH") # chunking too small
+      # !(study_site== "KAIBAB_HIGH" & file_name == "DOUBLE_ORIGINAL_HIGH")
+      # & !(study_site== "KAIBAB_HIGH" & file_name == "QUARTER_ORIGINAL_HIGH")
+      # & !(study_site== "KAIBAB_HIGH" & file_name == "QUARTER_ORIGINAL_OPTIMAL") # gets hung w 48m tot?
+      # & !(study_site== "KAIBAB_LOW" & file_name == "HALF_ORIGINAL_HIGH")
+      # & !(study_site== "N1" & file_name == "QUARTER_ORIGINAL_HIGH") # chunking too small
+      # & !(study_site== "WA85_02" & file_name == "DOUBLE_ORIGINAL_HIGH")
     ) %>%
     # dplyr::filter(
     #     (study_site== "KAIBAB_HIGH" & file_name == "DOUBLE_ORIGINAL_HIGH") 
